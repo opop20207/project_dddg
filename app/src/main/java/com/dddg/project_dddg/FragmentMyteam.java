8c0d2b73f518f4df8a15a6221bde0956a387191d
wbkim <kwb6675@naver.com>
2020-12-06 02:08:49 +0900

    2020106 FragmentMyteam

--- app/src/main/java/com/dddg/project_dddg/FragmentMyteam.java
+++ app/src/main/java/com/dddg/project_dddg/FragmentMyteam.java
@@ -1,16 +1,30 @@
 package com.dddg.project_dddg;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class FragmentMyteam extends Fragment {
+    ArrayList<TeamData> teamData = new ArrayList<TeamData>(Arrays.asList(new TeamData()));
     static FragmentMyteam instance;
     public FragmentMyteam() {
     }
@@ -29,7 +43,30 @@ public class FragmentMyteam extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_myteam,container,false);
+        ViewGroup rootview = (ViewGroup) inflater.inflate(R.layout.fragment_myteam,container,false);
+        Button button = (Button) rootview.findViewById(R.id.btn_myteam);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                final CharSequence[] teamName = {"1","2","3","4","5","6","7","8"};
+                final int[] hold = new int[1];
+
+                AlertDialog.Builder dlg = new AlertDialog.Builder(getContext(),
+                        android.R.style.Theme_DeviceDefault_Light_Dialog_Alert);
+
+                dlg.setTitle("팀 선택").setSingleChoiceItems(teamName, -1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        hold[0] = i;
+                    }
+                }).setNeutralButton("선택", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if(i>=0) Toast.makeText(getContext(), teamName[hold[0]], Toast.LENGTH_LONG).show();
+                    }
+                }).setCancelable(false).show();
+            }
+        });
+        return rootview;
     }
 
     @Override
---
+++ app/src/main/java/com/dddg/project_dddg/TeamData.java
@@ -0,0 +1,22 @@
+package com.dddg.project_dddg;
+
+public class TeamData {
+    public String team;
+    public String top;
+    public String jgl;
+    public String mid;
+    public String bot;
+    public String sup;
+
+    public  TeamData(){
+    }
+
+    public TeamData(String team, String top, String jgl, String mid, String bot, String sup) {
+        this.team = team;
+        this.top = top;
+        this.jgl = jgl;
+        this.mid = mid;
+        this.bot = bot;
+        this.sup = sup;
+    }
+}
