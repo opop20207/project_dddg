82e217fe29a0f2ed58369f2839faa3bbc565a560
opop20207 <opop202077@gmail.com>
2020-12-06 19:32:18 +0900

    image

--- app/src/main/java/com/dddg/project_dddg/TeamImgUrl.java
+++ app/src/main/java/com/dddg/project_dddg/TeamImgUrl.java
@@ -6,27 +6,17 @@ public class TeamImgUrl {
         String returnUrl = "";
         if(team_name==null) return returnUrl;
         switch (team_name){
-            case "Top Esports": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2F(%EA%B5%AC)CJEntus.png?alt=media&token=7ce77c91-5b9d-489f-aa96-fbf28e6bc533";
-                break;
-            case "(구)CJEntus": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2F(%EA%B5%AC)MKZ.png?alt=media&token=743afd16-860f-4139-9a32-d242164c2cf1";
-            break;
-            case "(구)MKZ": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2F(%EA%B5%AC)MKZ.png?alt=media&token=743afd16-860f-4139-9a32-d242164c2cf1";
-                break;
-            case "(구)TeamOP": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2F(%EA%B5%AC)TeamOP.png?alt=media&token=41927132-241c-4838-8974-bc0f0767334d";
+            case "Top Esports": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FTop%20Esports.png?alt=media&token=ef2c2694-7bbd-40ad-8654-0215dc6ba537";
                 break;
             case "Afreeca Freecs": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FAfreecaFreecs.jpg?alt=media&token=972f4c99-700f-4b0f-a33f-eeba19fc2dea";
                 break;
-            case "Arena": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FArena.png?alt=media&token=ff5c9296-5da5-475d-82be-85482c06c7be";
+            case "Arena": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FArena%20Quesito.png?alt=media&token=94794c09-076d-4d95-b267-461ed5287f3b";
                 break;
             case "BRIONBlade": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FBRIONBlade.jpg?alt=media&token=767e09e7-6422-4cc5-88b4-6719a487a5fa";
                 break;
             case "BigfileMiracle": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FBigfileMiracle.png?alt=media&token=a66dfcb4-a5f8-44b5-a8b5-9455fe874fd2";
                 break;
             case"CJEntus": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FCJEntus.jpg?alt=media&token=9e6ea214-78a6-44d9-a218-482d83839668";
-
-            case"CJEntusBlaze": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FCJEntusBlaze.jpg?alt=media&token=4cbd3886-7079-4ba9-9deb-21336edce8f2";
-            break;
-            case"CJEntusFrost": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FCJEntusFrost.jpg?alt=media&token=a23e8e5b-3131-4462-b163-f9088a30ad8b";
                 break;
             case"CLG": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FCLG.png?alt=media&token=e2725943-33cc-4ff6-b75c-1ab84ee69956";
                 break;
@@ -46,19 +36,7 @@ public class TeamImgUrl {
                 break;
             case"Hanwha Life Esports": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FHanwhaLifeEsports.png?alt=media&token=b8d5f3c6-bff8-4e5d-bd23-c7d459f47219";
                 break;
-            case"IM1팀": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FIM1%ED%8C%80.jpg?alt=media&token=1314cba8-7037-440c-897e-f560dce09263";
-                break;
-            case"IM2팀": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FIM2%ED%8C%80.jpg?alt=media&token=b2c0df06-cef7-4506-8f25-cdaf4f41d5b8";
-                break;
             case"Jin Air GreenWings": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FJinAirGreenWings.jpg?alt=media&token=672bb536-6b2a-4a31-a3c8-cc3d7b628a4b";
-                break;
-            case"Jin Air Falcons": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FJinAirGreenWingsFalcons.jpg?alt=media&token=96a24ea9-a05d-42d8-83ae-0f329a95b74e";
-                break;
-            case"Jin Air Stealths": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FJinAirGreenWingsStealths.jpg?alt=media&token=f88ee6ba-2ac4-46fe-9387-3966342fc2fc";
-                break;
-            case"KT롤스터Arrows": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FKT%EB%A1%A4%EC%8A%A4%ED%84%B0Arrows.jpg?alt=media&token=8b60c6e4-1cdd-43ea-aaa6-959480bdcb0a";
-                break;
-            case"KT롤스터Bullets": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FKT%EB%A1%A4%EC%8A%A4%ED%84%B0Bullets.jpg?alt=media&token=fc94b041-d30e-4a66-a6e8-fc08bd13cd1e";
                 break;
             case"kt Rolster": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FKT%EB%A1%A4%EC%8A%A4%ED%84%B0Bullets.jpg?alt=media&token=fc94b041-d30e-4a66-a6e8-fc08bd13cd1e";
                 break;
@@ -67,14 +45,6 @@ public class TeamImgUrl {
             case"PrimeITENJOY": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FPrimeITENJOY.jpg?alt=media&token=c46def1c-c585-4394-a2cd-97da2d00700b";
                 break;
             case"SANDBOX Gaming": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FSANDBOXGaming.jpg?alt=media&token=e8f668d7-6155-4c85-9d63-76e28f1549be";
-                break;
-            case"SK텔레콤T1K": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FSK%ED%85%94%EB%A0%88%EC%BD%A4T1K.jpg?alt=media&token=029f397a-acee-4ffb-b617-5fdcef5facbd";
-                break;
-            case"SK텔레콤T1S": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FSK%ED%85%94%EB%A0%88%EC%BD%A4T1S.jpg?alt=media&token=e8c369f3-93c2-444e-86af-12fd6bbe473a";
-                break;
-            case"Samsung Galaxy Blue": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FSamsungGalaxyBlue.png?alt=media&token=62da4af9-817a-4f2f-b77b-3335acdf612c";
-                break;
-            case"Samsung Galaxy White": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FSamsungGalaxyWhite.png?alt=media&token=65743e51-36d7-4596-a8fc-ae56b5893670";
                 break;
             case"Sbenu Korea": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FSbenuKorea.jpg?alt=media&token=f9b3b06e-c9b5-4ca8-a2b0-1ede428a82c5";
                 break;
@@ -94,12 +64,69 @@ public class TeamImgUrl {
                 break;
             case"ktRolster": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FktRolster.jpg?alt=media&token=b826e2ce-98a8-4153-bca1-a9522fbe961f";
                 break;
-            case"나진e-mFireShield": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2F%EB%82%98%EC%A7%84e-mFireShield.jpg?alt=media&token=661d89ad-9263-4d3a-802d-39c288cfddd3";
-            break;
-            case"나진e-mFireSword": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2F%EB%82%98%EC%A7%84e-mFireSword.jpg?alt=media&token=cbe29fbe-fcca-44dc-9f2a-5256fd5329b1";
-            break;
-
-            default: returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2F(%EA%B5%AC)CJEntus.png?alt=media&token=7ce77c91-5b9d-489f-aa96-fbf28e6bc533";
+            case "Adonis Marines": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FAdonis%20Marines.png?alt=media&token=1576f75f-4609-4a78-a43e-5265628ef040";
+                break;
+            case "Papara SuperMassive": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FPapara%20SuperMassive.png?alt=media&token=19405faf-9120-44b1-acac-669e265b4aad";
+                break;
+            case "Team Liquid": returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FTeam%20Liquid.png?alt=media&token=23287304-52b6-495a-b550-c7cc6a345afd";
+                break;
+            case "MAD Lions":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FMAD_Lions.png?alt=media&token=82c85f6b-69db-421c-935e-1aa796db6c9d";
+                break;
+            case "Legacy Esports":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FLegacy%20Esports.png?alt=media&token=49e63bb1-7894-4ec0-b2ed-7a6a148e1aff";
+                break;
+            case "INTZ":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FINTZ.png?alt=media&token=961bcdc1-b33c-478f-9583-ec325986903c";
+                break;
+            case "Rainbow7":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FRainbow7.png?alt=media&token=d5e031c7-896d-4879-a101-1921e592b8b4";
+                break;
+            case "LGD":
+            case "LGD Gaming":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FLGD%20Gaming.png?alt=media&token=65375335-6b9e-4e3d-8fd0-b71de7a35161";
+                break;
+            case "Unicorns of Love":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FUnicorns%20Of%20Love.png?alt=media&token=3caef46b-3393-44be-896b-a9c030cfadb9";
+                break;
+            case "FlyQuest":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FFlyQuest.png?alt=media&token=56d8c716-3aac-4788-a3d3-df649a4b6cee";
+                break;
+            case "Fnatic":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FFnatic.png?alt=media&token=82273346-84ef-4e94-a237-91ac447de994";
+                break;
+            case "Machi Esports":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FMachi%20Esports.png?alt=media&token=2e8a1e6f-07c4-42b8-950d-5d3f0e40a6e8";
+                break;
+            case "Team SoloMid":
+            case "TSM":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FTSM.png?alt=media&token=1fc8d5f9-bff9-4011-aa9e-01cf70534775";
+                break;
+            case "Suning":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FSuning.png?alt=media&token=ed14ae10-3d4d-4ba8-9211-6d76332df671";
+                break;
+            case "Rogue":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FRogue.png?alt=media&token=31113440-ac5d-46aa-832d-af6af5d9c2e6";
+                break;
+            case "FPX":
+            case "FunPlus Phoenix":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FFunPlus%20Phoenix.png?alt=media&token=9fccfa00-0579-43bc-aae9-26537b85c544";
+                break;
+            case "G2":
+            case "G2 Esports":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FG2%20Esports.png?alt=media&token=157ce99a-5f9a-4005-ac7e-7513f7bba54f";
+                break;
+            case "SK Telecom T1":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FSK%20Telecom%20T1.png?alt=media&token=cf3fd40c-f967-4365-823e-fd658b955329";
+                break;
+            case "Invictus Gaming":
+            case "IG":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FInvictus%20Gaming.png?alt=media&token=e20fd207-1b98-4b97-922f-388375ce960c";
+                break;
+            case "Splyce":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FSplyce.png?alt=media&token=4766034b-5dfd-44cb-ad74-79493db0e115";
+                break;
+            case "Ahq Esports Club":
+            case "ahqKorea":
+            case "ahq":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FAhq%20eSports%20Club.png?alt=media&token=9149e7a3-cb1f-4823-a54c-ab9271abcad7";
+                break;
+            case "Royal Never Give Up":
+            case "RNG":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FRoyal%20Never%20Give%20Up.png?alt=media&token=d32e196a-6381-462f-8059-6c62f076aebb";
+                break;
+            case "Clutch Gaming":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FClutch%20Gaming.png?alt=media&token=d3120d39-c9b3-4877-8464-54c948466a99";
+                break;
+            case "Hong Kong Attitude":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FHong%20Kong%20Attitude.png?alt=media&token=d13581e6-2c7c-4e08-81bc-499679dc46c4";
+                break;
+            case "Cloud 9":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FCloud9.png?alt=media&token=890b906e-a83b-490f-b6fc-cd000fc96206";
+                break;
+            case "J Team":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FJ%20Team.png?alt=media&token=50923415-4da1-4f98-85ce-dad39f024afd";
+                break;
+            case "GAM Esports":returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2FAdonis%20Marines.png?alt=media&token=1576f75f-4609-4a78-a43e-5265628ef040";
+                break;
+            default: returnUrl = "https://firebasestorage.googleapis.com/v0/b/projectdddg.appspot.com/o/Team_img%2F%ED%9B%88%EC%88%98%EC%A2%8B%EC%9D%80%EB%82%A0.png?alt=media&token=0757e50e-3db5-4f7f-81b0-4262cffbfad6";
                 break;
         }
         return returnUrl;
